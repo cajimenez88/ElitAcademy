5d03716394f07a7b50b5c4271b49bad7c60fbb77
Erlynvelez <rubenerlyn@gmail.com>
2024-01-13 18:31:19 -0500

    Create lista.component.js

---
+++ out-tsc/app/app/lista/lista.component.js
@@ -0,0 +1,13 @@
+import { __decorate } from "tslib";
+import { Component } from '@angular/core';
+let ListaComponent = class ListaComponent {
+};
+ListaComponent = __decorate([
+    Component({
+        selector: 'app-lista',
+        templateUrl: './lista.component.html',
+        styleUrl: './lista.component.css'
+    })
+], ListaComponent);
+export { ListaComponent };
+//# sourceMappingURL=lista.component.js.map
