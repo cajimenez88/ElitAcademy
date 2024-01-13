29e9a5c1a9786aa2c867044233e1654f24c7ddaa
Erlynvelez <rubenerlyn@gmail.com>
2024-01-13 18:55:28 -0500

    Create banner-principal.component.js

---
+++ out-tsc/app/app/banner-principal/banner-principal.component.js
@@ -0,0 +1,13 @@
+import { __decorate } from "tslib";
+import { Component } from '@angular/core';
+let BannerPrincipalComponent = class BannerPrincipalComponent {
+};
+BannerPrincipalComponent = __decorate([
+    Component({
+        selector: 'app-banner-principal',
+        templateUrl: './banner-principal.component.html',
+        styleUrl: './banner-principal.component.css'
+    })
+], BannerPrincipalComponent);
+export { BannerPrincipalComponent };
+//# sourceMappingURL=banner-principal.component.js.map
