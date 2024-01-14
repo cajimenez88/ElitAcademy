0794c0b74b354d1edac57354a8f4786387bbd339
dielis21 <156273731+dielis21@users.noreply.github.com>
2024-01-13 19:22:26 -0500

    Create card-titulo.component.ts

---
+++ src/app/card-titulo/card-titulo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-titulo',
+  templateUrl: './card-titulo.component.html',
+  styleUrl: './card-titulo.component.css'
+})
+export class CardTituloComponent {
+
+}
