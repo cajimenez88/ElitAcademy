7a859bd02dd65eebafb3d4e2f67c3c9701bb34a4
Erlynvelez <rubenerlyn@gmail.com>
2024-01-21 02:34:31 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardTituloComponent } from './card-titulo/card-titulo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Otras rutas pueden ir aquí
+  { path: 'card-titulo', component: CardTituloComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
