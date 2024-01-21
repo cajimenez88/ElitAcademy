96a57d15b5f977b851dbfada4544b563bbff0b39
Erlynvelez <rubenerlyn@gmail.com>
2024-01-21 03:59:24 -0500

    Cambios Erlyn

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -13,8 +13,17 @@ menucursos=[{link:"#", nombre:"Ilustrator para principiantes"},
     {link:"#", nombre:"Programación para niños Scratch"}];
     mostrarcurso(curso:any){
         alert("Has elegido el siguiente curso "+ curso.nombre)
+    }
+    // Variable para controlar si la barra de navegación debe ser pegajosa
+    isNavbarSticky: boolean = false;
 
-
-
+    // Método que se llama cuando ocurre el evento de scroll
+    @HostListener('window:scroll', ['$event'])
+    onScroll(event: Event): void {
+        // Ajustar el valor de isNavbarSticky según la posición de scroll
+        this.isNavbarSticky = window.scrollY > 0;
+    }
+    mostrarMensaje(): void {
+        window.alert("Estamos en construcción");
     }
 }
